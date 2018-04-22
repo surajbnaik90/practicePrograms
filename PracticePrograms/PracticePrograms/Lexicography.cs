c97297385cfbeaed0db40d8da9ff9e83577adb68
Suraj Naik <suraj.b.naik@gmail.com>
2018-04-22 13:54:24 -0700

    Lexicography program

---
+++ PracticePrograms/PracticePrograms/Lexicography.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePrograms
+{
+    //You have been given a task of reordering some data from a log file. Every line in the log file
+    //is a space delimated list of strings and all lines begin with an identifier that is alphanumeric.
+    //After the identifier, a line will consist of either a list of words using only English letters or only 
+    //a list of integers. There will be no line that consists of only a n identifier
+    class Lexicography
+    {
+    }
+}
